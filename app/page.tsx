5ffef8e0e08952601fdc13f21332c7455af3cb4c
Dorian-Caillat <caillatdorian@gmail.com>
2024-08-27 13:43:46 +0200

    spotlight setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center 
     items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7x1 w-full">
-        <h1>Bonjour, Portfolio!</h1>
+        <Hero />
       </div>
     </main>
   );
